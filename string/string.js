71046f41664956f41b903cca8965db0692ec0c17
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-05-06 18:13:00 +0530

    Create string.js

---
+++ string/string.js
@@ -0,0 +1,8 @@
+let name1="bro code is awsome";
+console.log(name1.replace("bro","jeevith"));
+console.log(name1.toLowerCase());
+console.log(name1.toUpperCase());
+console.log(name1.indexOf(" "));
+console.log(name1.lastIndexOf("o"));
+console.log(name1.repeat(4));
+console.log(name1.search("b"));
